223f77101a24f21705b9f63ba49f1121640f66ad
Papa Smerf <albert.lis@interia.pl>
2019-03-18 21:59:55 +0100

    Split sensor from value

--- Python/Reading from arduino/main.py
+++ Python/Reading from arduino/main.py
@@ -1,11 +1,15 @@
 import serial
-import serial.tools.list_ports as port_list
+#import serial.tools.list_ports as port_list
 
-ports = list(port_list.comports())
-for p in ports: 
-    print(p)
+#ports = list(port_list.comports())
+#for p in ports: 
+#    print(p)
 
-ser = serial.Serial('com4', 9600)
-arduinoData = str(ser.readline())
-print(arduinoData[2:][:-5])
-print("\n")
+ser = serial.Serial('com4', 115200)
+while True:
+    arduinoData = str(ser.readline())
+    splitedData = (arduinoData[2:][:-5]).split()
+    sensorName = splitedData[0]
+    sensorValue = int(splitedData[1])
+    print(sensorName, sensorValue)
+    #print("\n")
